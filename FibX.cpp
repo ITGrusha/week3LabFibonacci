7246bebcaaa503c7ccca73319a24cd8c41e26a2b
Grusha5480 <MrGraphitem@gmail.com>
2019-11-25 01:24:03 +0200

    code tiding

--- FibX.cpp
+++ FibX.cpp
@@ -9,17 +9,17 @@ FibX::FibX(long long keyLength) {
         throw SEQUENCE_DEPTH_EXCEPTION();
     }
     this->keyLength = keyLength;
-    fibVector = std::vector<long long>(keyLength - 1, 0);
-    fibVector.emplace_back(1);
+    fibVector = std::vector<long long>(keyLength - 1LL, 0LL);
+    fibVector.emplace_back(1LL);
 }
 
 long long FibX::GetNextElement() {
-    fibVector.emplace_back(std::accumulate(fibVector.begin(), fibVector.end(), 0));
+    fibVector.emplace_back(std::accumulate(fibVector.begin(), fibVector.end(), 0LL));
     fibVector.erase(fibVector.begin());
     return *fibVector.begin();
 }
 
 void FibX::reset() {
-    fibVector = std::vector<long long>(keyLength - 1, 0);
-    fibVector.emplace_back(1);
+    fibVector = std::vector<long long>(keyLength - 1LL, 0LL);
+    fibVector.emplace_back(1LL);
 }
